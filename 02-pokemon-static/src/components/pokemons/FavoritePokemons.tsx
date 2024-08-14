35bf554163e138a671e035dd2af9848259a43eb5
Sergio Arce <arce.roldan.sergio@gmail.com>
2024-08-14 14:01:44 -0600

    Página de pokemon-static terminada

---
+++ 02-pokemon-static/src/components/pokemons/FavoritePokemons.tsx
@@ -0,0 +1,24 @@
+import { FavoritePokemonCard } from "@components/pokemons/FavoritePokemonCard";
+
+import type { FavoritePokemon } from "@interfaces/favorite-pokemon"
+import { For, createSignal } from "solid-js"
+
+const getLocalStoragePokemons =( ): FavoritePokemon[] => {
+    const favoritePokemons = JSON.parse(
+        localStorage.getItem('favorites') ?? "[]"
+    )
+    return favoritePokemons
+}
+
+
+export const FavoritePokemons = (  ) => {
+    const [ pokemons, setPokemons ] = createSignal( getLocalStoragePokemons() );
+
+    return (
+        <div class="grid grid-cols-2 sm:grid-cols-4">
+            <For each={ pokemons() }>
+                { ( pokemon ) => <FavoritePokemonCard pokemon={pokemon} /> }
+            </For>
+        </div>
+    )
+}
