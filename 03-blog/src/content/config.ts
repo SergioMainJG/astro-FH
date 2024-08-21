a64363564ee66fb756ef14ef47afb01e2e4366e3
Sergio Arce <arce.roldan.sergio@gmail.com>
2024-08-21 16:18:33 -0600

    Casi fin de la sección 7

--- 03-blog/src/content/config.ts
+++ 03-blog/src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
     type:   "content",
@@ -6,14 +6,28 @@ const blogCollection = defineCollection({
                 title: z.string(),
                 date: z.date(),
                 description: z.string(),
-                author: z.string(),
                 image: image().refine(( img ) => img.width < 1200, {
                     message: 'Image should be lower than 1200px',
                 }),
-                tags: z.array( z.string() )
+
+                // author: z.string(),
+                author: reference('author'),
+                
+                tags: z.array( z.string() ),
+
+                isDraft: z.boolean().default( false),
             }),
 });
 
+const authorCollection = defineCollection({
+    type:   "data",
+    schema: ({ image }) => z.object({
+            name: z.string(),
+            avatar: image(),
+    })
+})
+
 export const collections = {
     blog: blogCollection,
+    author: authorCollection,
 };
